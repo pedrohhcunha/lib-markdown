8a613e0e275d41f9dfb9a91d9410a58924f97844
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-03-29 20:21:38 -0300

    Criando função inciial para buscar um arquivo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("This is my program running")
+//Importando o módulo fs, para lidar com arquivos
+const fs = require('fs')
+
+//Criando função para ler o arquivode um post
+const getFile = (fileName) => {
+
+    //Lendo arquivo de um post no diretório posts
+    fs.readFile(fileName, 'utf8', (error, data) => {
+
+        //Se houver erro, retorna o erro
+        if (error) throw new Error(error)
+        
+        console.log("Texto do post: ", data)
+    })
+}
+
+//Executando a função de busca do arquivo
+getFile('./files/post.md')
