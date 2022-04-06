80013fd6fa872df9a7ad2b645a7b16856c98cb78
pedrohhcunha <pedro.cunha@onfinity.com.br>
2022-04-06 13:27:18 -0300

    Adding error tests

--- src/test/main.test.js
+++ src/test/main.test.js
@@ -4,4 +4,18 @@ describe('main::', () => {
     it('should return true if pahtName is a file', async () => {
        expect(await main('./src/files/test.md')).toBe(true)
     })
+    it('should return error when pathName does not passed', async () => {
+        try{
+            await main()
+        } catch(error){
+            expect(error.message).toBe('O PathName não existe!')
+        }
+    })
+    it('should return error when pathName is a folder', async () => {
+        try{
+            await main('./src/files/')
+        } catch(error){
+            expect(error.message).toBe('O PathName não representa um arquivo!')
+        }
+    })
 });
